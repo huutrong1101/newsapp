448ee293ce882eda0600caa5058df71b10a31c29
Nguyễn Hữu Trọng <75382199+huutrong1101@users.noreply.github.com>
2023-02-15 18:04:07 +0700

    upd: method

--- requestMethods.js
+++ requestMethods.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://nguyenhuutrong.cyclic.app/api";
 
 export const domainApi = BASE_URL;
 
